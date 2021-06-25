ae95e3e5b62dcfd4a9dd0ed917d64df9769ce1dc
xizh <zhihongxie@xiaoe-tech.com>
2021-06-25 17:26:23 +0800

    初始化改动

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ function element (tag, options, container) {
   const cls = options.class
   const text = options.text
   const html = options.html
+  const id = options.id
 
   const el = document.createElement(tag)
+  el.id = id;
   el.classList.add(cls)
   if (container != null) {
     container.appendChild(el)
@@ -32,7 +34,9 @@ module.exports = {
   blocks: {
     api: {
       process (block) {
+        console.log('api block',block);
         return this.book.renderBlock('markdown', block.body).then(function (body) {
+          console.log('api block body',body);
           // Create container
           const container = element('div', { class: 'api-container' })
 
@@ -42,12 +46,79 @@ module.exports = {
           element('h2', { text: block.args[0] }, header)
           if (block.kwargs.url) { element('span', { text: block.kwargs.url }, header) }
 
+
           // Create content section
           const content = element('div', { class: 'api-content' }, container)
           element('div', { class: 'api-description', html: body }, content)
           return container.outerHTML
         })
+      },
+      blocks:[
+        {
+          parameters: {
+            process (block) {
+              console.log('parameters block',block);
+              return this.book.renderBlock('markdown', block.body).then(function (body) {
+                console.log('parameters block body',body);
+                // Create container
+                const container = element('div', { class: 'params-container' })
+      
+                if(block.kwargs.params)  element('textarea',{id: 'urlParams', value: JSON.stringify(block.kwargs.params || {})}, container)
+                if(block.kwargs.data) element('textarea',{id: 'bodyParams', value: JSON.stringify(block.kwargs.data || {})}, container)
+      
+                console.log(container.outerHTML);
+                return container.outerHTML
+              })
+            }
+          },
+        },
+        {
+          response: {
+            process (block) {
+              console.log('response block',block);
+              return this.book.renderBlock('markdown', block.body).then(function (body) {
+                console.log('response block body',body);
+                // Create container
+                const container = element('div', { class: 'response-container' })
+      
+                // Create inner section
+                element('div', { class: 'response-inner' }, container)
+                return container.outerHTML
+              })
+            }
+          },
+        }
+      ]
+    },
+    parameters: {
+      process (block) {
+        console.log('parameters block',block);
+        return this.book.renderBlock('markdown', block.body).then(function (body) {
+          console.log('parameters block body',body);
+          // Create container
+          const container = element('div', { class: 'params-container' })
+
+          if(block.kwargs.params)  element('textarea',{id: 'urlParams', value: JSON.stringify(block.kwargs.params || {})}, container)
+          if(block.kwargs.data) element('textarea',{id: 'bodyParams', value: JSON.stringify(block.kwargs.data || {})}, container)
+
+          console.log(container.outerHTML);
+          return container.outerHTML
+        })
       }
-    }
+    },
+    response: {
+      process (block) {
+        console.log('response block',block);
+        return this.book.renderBlock('markdown', block.body).then(function (body) {
+          console.log('response block body',body);
+          // Create container
+          const container = element('div', { class: 'response-container' })
+
+          // Create inner section
+          element('div', { class: 'response-inner' }, container)
+          return container.outerHTML
+        })
+      }
+    },
   }
 }
